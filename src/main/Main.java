c96868eacd91fb6f95de218f42bc56d6b9d23cfb
HugoDarthenay <120332375+HugoDarthenay@users.noreply.github.com>
2024-01-08 16:07:43 +0100

    Delete src directory

--- src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Bonjour");
-		System.out.println("test-1");
-		System.out.println("test-2");
-	}
-}
